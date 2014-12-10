003379ea8d9b93fcf7d9f66374cdb51140109c6b
joanmartinezvalles <joanmartinezvalles@gmail.com>
2014-12-10 19:25:11 +0100

    Separado

--- JuegoAhorcado/AhorcadoV1.cs
+++ JuegoAhorcado/AhorcadoV1.cs
@@ -3,78 +3,101 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Logica;
 
 namespace JuegoAhorcado
 {
     class AhorcadoV1
     {
-        static void Main()
+        public static void Main()
         {
-            //1. El programa escoge una palabra al azar de una lista..
-            string[] palabras = { "gato", "perro", "restaurante", "ordenador", "ventana" };
-            Random generadorAleatorio = new Random();
-            int numAzar = generadorAleatorio.Next(0, palabras.Length);
-            string palabraAdivinar = palabras[numAzar];
+            Logica logica = new Logica();
 
-            //2.Se muestra una serie de guiones.
-            string palabraMostrar = "";
-            for (int i = 0; i < palabraAdivinar.Length; i++)
-            {
-                //if (palabraAdivinar[i] == ' ')
-                //    palabraMostrar += " ";
+            string palabraAdivinar = logica.ListaPalabras();
 
-                palabraMostrar += "_ ";
-            }
+            string palabraMostrar = logica.PalabraOculta(palabraAdivinar);
 
-            int fallosRestantes = 10;
-            char letraActual;
-            bool terminado = false;
-
-            do
-            {
-                //3.Se muestra la palabra oculta y el jugador elige una letra.
-                Console.WriteLine("Palabra oculta: {0}", palabraMostrar);
-                Console.WriteLine("Fallos restantes: {0}", fallosRestantes);
-
-                //4.El usuario elige una letra.
-                Console.Write("Introduzca una letra: ");
-                letraActual = Convert.ToChar(Console.ReadLine());
-
-                //5.Si letra no pertenece a la plabra pierde un intento.
-                if (palabraAdivinar.IndexOf(letraActual) == -1)
-                    fallosRestantes--;
-
-                //6.Si la letra es parte de la palabra, la letra se muestra como parte de ella.
-                string palabraOculta ="";
-                palabraMostrar = palabraMostrar.Replace(" ", "");
-
-                    for (int i = 0; i < palabraAdivinar.Length; i++)
-                    {
-                        if (letraActual == palabraAdivinar[i]) // equivalente letraActual==palabraAdivinar[i]
-                            palabraOculta += letraActual + " ";
-                        else if (palabraMostrar[i] != '_')
-                            palabraOculta += palabraMostrar[i] + " ";
-                        else
-                            palabraOculta += "_ ";
-                    }
-                    palabraMostrar = palabraOculta;
-                //7.Comprobar si el juego ha terminado.
-                if (palabraMostrar.IndexOf("_ ") < 0)
-                {
-                    Console.WriteLine("Felicidades has acertado ({0})", palabraAdivinar);
-                    terminado = true;
-                }
-                if (fallosRestantes == 0)
-                {
-                    Console.WriteLine("Lo siento. La palabra era {0}", palabraAdivinar);
-                    terminado = true;
-                }
-
-                Console.WriteLine();
-            }
-            while (!terminado);
+            palabraMostrar = logica.JuegoAhorcado(palabraAdivinar, palabraMostrar);
 
             Console.ReadKey();
         }
+
+        //private static string JuegoAhorcado(string palabraAdivinar, string palabraMostrar)
+        //{
+        //    int fallosRestantes = 10;
+        //    char letraActual;
+        //    bool terminado = false;
+
+        //    do
+        //    {
+        //        //3.Se muestra la palabra oculta y el jugador elige una letra.
+        //        Console.WriteLine("\nPalabra oculta: {0}", palabraMostrar);
+        //        Console.WriteLine("\nFallos restantes: {0}", fallosRestantes);
+
+
+        //        //4.El usuario elige una letra.
+        //        Console.Write("Introduzca una letra: ");
+        //        letraActual = Convert.ToChar(Console.ReadLine());
+
+        //        //5.Si letra no pertenece a la plabra pierde un intento.
+        //        if (palabraAdivinar.IndexOf(letraActual) == -1)
+        //            fallosRestantes--;
+
+        //        //6.Si la letra es parte de la palabra, la letra se muestra como parte de ella.
+        //        string palabraOculta = "";
+        //        palabraMostrar = palabraMostrar.Replace(" ", "");
+
+        //        for (int i = 0; i < palabraAdivinar.Length; i++)
+        //        {
+        //            if (letraActual == palabraAdivinar[i]) // equivalente letraActual==palabraAdivinar[i]
+        //                palabraOculta += letraActual + " ";
+        //            else if (palabraMostrar[i] != '_')
+        //                palabraOculta += palabraMostrar[i] + " ";
+        //            else
+        //                palabraOculta += "_ ";
+        //        }
+        //        palabraMostrar = palabraOculta;
+        //        //7.Comprobar si el juego ha terminado.
+        //        if (palabraMostrar.IndexOf("_ ") < 0)
+        //        {
+        //            Console.WriteLine("Felicidades has acertado ({0})", palabraAdivinar);
+        //            terminado = true;
+        //        }
+        //        if (fallosRestantes == 0)
+        //        {
+        //            Console.WriteLine("Lo siento. La palabra era {0}", palabraAdivinar);
+        //            terminado = true;
+        //        }
+
+        //        Console.WriteLine();
+        //    }
+        //    while (!terminado);
+        //    return palabraMostrar;
+        //}
+
+        //private static string PalabraOculta(string palabraAdivinar)
+        //{
+        //    //2.Se muestra una serie de guiones.
+        //    string palabraMostrar = "";
+        //    for (int i = 0; i < palabraAdivinar.Length; i++)
+        //    {
+        //        //if (palabraAdivinar[i] == ' ')
+        //        //    palabraMostrar += " ";
+
+        //        palabraMostrar += "_ ";
+        //    }
+        //    return palabraMostrar;
+        //}
+
+        //private static string ListaPalabras()
+        //{
+        //    //1. El programa escoge una palabra al azar de una lista..
+        //    string[] palabras = { "gato", "perro", "restaurante", "ordenador", "ventana", "libro", "persona", "metodo", "variable",
+        //                        "palabra", "letra", "oficina", "boligrafo", "auriculares", "altavoz"};
+        //    Random generadorPalabras = new Random();
+        //    int numAzar = generadorPalabras.Next(0, palabras.Length);
+        //    string palabraAdivinar = palabras[numAzar];
+        //    return palabraAdivinar;
+        //}
     }
 }
---
+++ JuegoAhorcado/LogicaAhorcado.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuegoAhorcado
+{
+    class Logica
+    {
+
+        public string ListaPalabras()
+        {
+            //1. El programa escoge una palabra al azar de una lista..
+            string[] palabras = { "gato", "perro", "restaurante", "ordenador", "ventana", "libro", "persona", "metodo", "variable",
+                                "palabra", "letra", "oficina", "boligrafo", "auriculares", "altavoz"};
+            Random generadorPalabras = new Random();
+            int numAzar = generadorPalabras.Next(0, palabras.Length);
+            string palabraAdivinar = palabras[numAzar];
+            return palabraAdivinar;
+        }
+
+        public string PalabraOculta(string palabraAdivinar)
+        {
+            //2.Se muestra una serie de guiones.
+            string palabraMostrar = "";
+            for (int i = 0; i < palabraAdivinar.Length; i++)
+            {
+                //if (palabraAdivinar[i] == ' ')
+                //    palabraMostrar += " ";
+
+                palabraMostrar += "_ ";
+            }
+            return palabraMostrar;
+        }
+        public string JuegoAhorcado(string palabraAdivinar, string palabraMostrar)
+        {
+            int fallosRestantes = 10;
+            char letraActual;
+            bool terminado = false;
+
+            do
+            {
+                //3.Se muestra la palabra oculta y el jugador elige una letra.
+                Console.WriteLine("\nPalabra oculta: {0}", palabraMostrar);
+                Console.WriteLine("\nFallos restantes: {0}", fallosRestantes);
+
+                //4.El usuario elige una letra.
+                Console.Write("Introduzca una letra: ");
+                letraActual = Convert.ToChar(Console.ReadLine());
+
+                //5.Si letra no pertenece a la plabra pierde un intento.
+                if (palabraAdivinar.IndexOf(letraActual) == -1)
+                    fallosRestantes--;
+
+                //6.Si la letra es parte de la palabra, la letra se muestra como parte de ella.
+                string palabraOculta = "";
+                palabraMostrar = palabraMostrar.Replace(" ", "");
+
+                for (int i = 0; i < palabraAdivinar.Length; i++)
+                {
+                    if (letraActual == palabraAdivinar[i]) // equivalente letraActual==palabraAdivinar[i]
+                        palabraOculta += letraActual + " ";
+                    else if (palabraMostrar[i] != '_')
+                        palabraOculta += palabraMostrar[i] + " ";
+                    else
+                        palabraOculta += "_ ";
+                }
+                palabraMostrar = palabraOculta;
+                //7.Comprobar si el juego ha terminado.
+                if (palabraMostrar.IndexOf("_ ") < 0)
+                {
+                    Console.WriteLine("Felicidades has acertado ({0})", palabraAdivinar);
+                    terminado = true;
+                }
+                if (fallosRestantes == 0)
+                {
+                    Console.WriteLine("Lo siento. La palabra era {0}", palabraAdivinar);
+                    terminado = true;
+                }
+
+                Console.WriteLine();
+            }
+            while (!terminado);
+            return palabraMostrar;
+        }
+    }
+}
+
